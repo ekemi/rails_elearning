e79f5ab2629ca0face5b7268faf175910389fe3a
Yapnounkie <edmundopierre@yahoo.com>
2020-12-01 02:49:02 -0800

    create devise User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :students
   has_many :courses, through: :students
   has_many :reviews, through: :students
